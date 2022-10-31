7ec91c81f1bfc73b32a40f72266d32398e868e69
NazarIvankevych <nazar.ivankevych@gmail.com>
2022-10-31 23:15:17 +0200

    Create a dag with few tasks: check api, create table, extract user, process_user. Need to fix issues with check api.

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -23,37 +23,40 @@ def _process_user(ti):
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=None)
     
 
-with DAG('user_processing', start_date=datetime(2022, 1, 12),
+with DAG('user_processing', start_date=datetime(2022, 1, 1),
         schedule_interval='@daily', catchup=False) as dag:
-            create_table = PostgresOperator(
-                    task_id='create_table',
-                    postgres_conn_id='postgres',
-                    sql='''
-                        CREATE TABLE IF NOT EXISTS users (
-                        firstname TEXT NOT NULL,
-                        lastname TEXT NOT NULL,
-                        country TEXT NOT NULL,
-                        username TEXT NOT NULL,
-                        password TEXT NOT NULL,
-                        email TEXT NOT NULL
-                        );
-                    ''')
-            is_api_available = HttpSensor(
-            task_id='is_api_available',
-            http_conn_id='user_api',
-            endpoint='api/'
-            )
-            
-            extract_user = SimpleHttpOperator(
-                task_id='extract_user',
-                http_conn_id='user_api',
-                endpoint='api/',
-                method='GET',
-                response_filter=lambda response: json.loads(response.text),
-                log_response=True
-            )
-            
-            process_user = PythonOperator(
-                task_id='process_user',
-                python_callable=_process_user,
-            )
+    # Create a table of Postgres DB with connection
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users (
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL
+            );
+        '''
+    )
+    # Create a task which verified if API is available or not with HttpSensor
+    is_api_available = HttpSensor(
+        task_id='is_api_available',
+        http_conn_id='user_api',
+        endpoint='api/'
+    )
+    
+    extract_user = SimpleHttpOperator(
+        task_id='extract_user',
+        http_conn_id='user_api',
+        endpoint='api/',
+        method='GET',
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True
+    )
+    
+    process_user = PythonOperator(
+        task_id='process_user',
+        python_callable=_process_user,
+    )
