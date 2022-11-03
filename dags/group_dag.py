4e4cdfb917b4e862886503ee050df32becbb2797
NazarIvankevych <58091951+NazarIvankevych@users.noreply.github.com>
2022-11-03 22:25:07 +0200

    Features test (#2)
    * Check all files and a new added
    
    * Check all files and a new added
    
    * Final files
    
    * Fix issues with HEAD
    
    * Add .gitignore file, new tasks, fix issues with errors
    
    * Check if gitignore work properly
    
    * Add file to gitignore, create a new DAG for grouping.
    
    * Create a new subdag transform and rewrite a chain of processes.

--- dags/group_dag.py
+++ dags/group_dag.py
@@ -1,10 +1,30 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+# import subdag operator for working with additional dags
+from airflow.operators.subdag import SubDagOperator
+from subdags.subdag_downloads import subdag_downloads
+from subdags.subdag_transforms import subdag_transforms
 
 from datetime import datetime
 
 with DAG('group_dag', start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
+    # add args with schedule of working with subdag
+    args = {
+        'start_date': dag.start_date,
+        'schedule_interval': dag.schedule_interval,
+        'catchup': dag.catchup
+    }
+
+    downloads = SubDagOperator(
+        task_id='downloads',
+        subdag=subdag_downloads(dag.dag_id, 'downloads', args)
+    )
+    
+    transforms = SubDagOperator(
+        task_id='transforms',
+        subdag=subdag_transforms(dag.dag_id, 'transforms', args)
+    )
 
     download_a = BashOperator(
         task_id='download_a',
@@ -25,6 +45,8 @@ with DAG('group_dag', start_date=datetime(2022, 1, 1),
         task_id='check_files',
         bash_command='sleep 10'
     )
+
+    downloads >> check_files >> transforms
 
     transform_a = BashOperator(
         task_id='transform_a',
